6f611ec8f19c83803e3fc322773762daf8c56c32
KrisnaMahardika <gedekrisna119@gmail.com>
2024-07-11 15:21:27 +0800

    fix update text

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something For U,Trisna'.split('')
+const text = 'I Have Something For U, Trisna'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
